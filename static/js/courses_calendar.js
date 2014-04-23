306ad4c1ae31ed365c6736b522b8a40d9461357c
mszymulanski <mszymulanski@gmail.com>
2014-04-23 14:59:14 -0500

    Refactoring: added function for changing color of course in calendar, the color depends on college which offers that class. Cleaned up the course calendar script.

--- static/js/courses_calendar.js
+++ static/js/courses_calendar.js
@@ -1,45 +1,8 @@
-function addCourse(day_column_id) 
-{
-	var o1 = {code: "MATH", subject: "Calc III", crn: "10145", startTime: '01:00 PM', endTime: '01:50 PM', days: 'MWF '}; // temporary class info object
-	
-	var day_col = document.getElementById(day_column_id);
-	var newChild = createCourse(o1, day_col);
-	day_col.appendChild(newChild);
-	
-	// set all day_times height to days_column
-	//console.log(day_col.offsetHeight);
-	var aDayTimesElements = document.getElementsByClassName('aday_times');
-
-}
-
-function createCourse(courseInfo, parentNode)
-{
-	var course = document.createElement('div');
-	course.setAttribute('id', "course_" + courseInfo.crn);
-	course.className = "course_entry";
-	course.style.height = '30px';
-	course.style.top = '449.5px';
-	course.innerHTML = courseInfo.subject;
-	
-	// create remove button
-	var button = document.createElement('button');
-	course.appendChild(button);
-	button.setAttribute('type', 'button');
-	button.setAttribute('value', 'remove course ' + courseInfo.crn);
-	button.setAttribute('title', "Remove course " + courseInfo.subject + " that is on " + courseInfo.days + ".");
-	button.innerHTML = '-';
-	
-	button.onclick = function() {	   
-	    parentNode.removeChild(course);
-	    
-	    return true;
-	  };
-
-	return course;
-}
-
-
-
+/**
+*	This function generate from course information an HTML div with that can be added to a calendar. 
+*	@param associated array of course info values
+*	@return array of DOM objects that represent course from given information
+*/
 function creteCourseDiv(courseInfo)
 {
 	var parents = dayTokenStringToDaysTimesIds(courseInfo.days);
@@ -50,11 +13,12 @@ function creteCourseDiv(courseInfo)
 		// create div for the course
 		var course = document.createElement('div');
 		var courseId = "course_" + courseInfo.crn + "_" + parents[ i ];
+		
 		course.setAttribute('id', courseId);
 		course.className = "course_entry";
 		course.style.height = getCourseBoxHeight(courseInfo.startTime, courseInfo.endTime) + "px";
 		course.style.top = yCoordFromHour(courseInfo.startTime).toString() + "px";
-		
+		course.style.backgroundColor = getCourseColor(courseInfo.code); 
 		course.innerHTML = courseInfo.subject;
 		
 		children.push(course);
@@ -76,7 +40,6 @@ function creteCourseDiv(courseInfo)
 			{
 				var parent = document.getElementById(parentsIds[ i ]);
 				parent.removeChild(children[ i ]);
-				console.log("remove " + i);
 			}
 	    
 			return true;
@@ -88,7 +51,11 @@ function creteCourseDiv(courseInfo)
 	return children;
 }
 
-
+/**
+*	This function adds to calendar course from given course information.
+* 	@param associated array of course info values
+* 	@return boolean does not indicate anything - exists only to make browser happy
+*/
 function addToCalendar(courseInfo)
 {
 	var children = creteCourseDiv(courseInfo);
@@ -102,7 +69,6 @@ function addToCalendar(courseInfo)
 	
 	return true;
 }
-
 
 /**
  * This function checks if the time string is in the correct format. 
@@ -280,4 +246,113 @@ function getCourseBoxHeight(startTime, endTime)
 	}
 	
 	return boxWidth;
+}
+
+/**
+ * This function generates a RGBA color string appropriate to given course code.
+ * @param courseCode string capitalized course code
+ * @returns {string} string for CSS color value in RGBA format 'RGBA(hex, hex, hex, float)'
+ */
+function getCourseColor(courseCode)
+{
+	// Agricultural, Consumer and Environmental Sciences
+	if( ("ABE" === courseCode) || ("ACE" === courseCode) || ("ACES" === courseCode) || ("AGED" === courseCode) || ("ANSC" === courseCode) || ("CPSC" === courseCode) || ("FSHN" === courseCode) || ("HDFS " === courseCode) || ("HORT" === courseCode)  || ("NRES" === courseCode) || ("NUTR" === courseCode) || ("PLPA" === courseCode) || ("RSOC" === courseCode) || ("TSM" === courseCode) )
+	{
+		return "rgba(0,245,45,0.8)";
+	}
+	// Applied Health Sciences
+	else if( ("AHS" === courseCode) || ("CHLH" === courseCode) || ("IHLT" === courseCode) || ("KIN" === courseCode) || ("REHB" === courseCode) || ("RST" === courseCode) || ("SHS" === courseCode) )
+	{
+		return "rgba(170,156,255,0.8)"; //"#AA9CFF";
+	}
+	// Armed Forces
+	else if( ("AFAS " === courseCode) || ("MILS" === courseCode) || ("NS" === courseCode) )
+	{
+		return "rgba(143,58,6,0.8)";
+	}
+	// Institute of Aviation
+	else if( ("AVI" === courseCode) )
+	{
+		return "rgba(138,253,255,0.8)"; //"#8AFDFF";
+	}
+	// College of Business
+	else if( ("ACCY" === courseCode) || ("BADM" === courseCode) || ("BUS" === courseCode) || ("FIN" === courseCode) || ("MBA" === courseCode) )
+	{
+		return "rgba(159,178,179,0.8)"; //"#9FB2B3";
+	}
+	// Education
+	else if( ("CI" === courseCode) || ("EDPR" === courseCode) || ("EOL" === courseCode) || ("EPS" === courseCode) || ("EPSY" === courseCode) || ("HRD" === courseCode)  || ("HRE" === courseCode)  || ("SPED" === courseCode) )
+	{
+		return "rgba(0,252,126,0.8)"; //"#00FC7E";
+	}
+	// Engineering 
+	else if( ("AE" === courseCode) || ("BIOE" === courseCode) || ("CEE" === courseCode) || ("CS" === courseCode) || ("CSE" === courseCode) || ("ECE" === courseCode)  || ("ENG" === courseCode)  || ("GE" === courseCode)  || ("IE" === courseCode) || ("ME" === courseCode) || ("MSE" === courseCode) || ("NPRE " === courseCode) || ("PHYS" === courseCode) || ("TAM" === courseCode) || ("TE" === courseCode) || ("TMGT" === courseCode) )
+	{
+		return "rgba(252,202,0,0.8)"; //"#FCCA00";
+	}
+	// Environmental Council
+	else if( ("ENVS" === courseCode) )
+	{
+		return "rgba(94,94,94,0.8)"; //"#5E5E5E";
+	}
+	// Fine and Applied Arts
+	else if( ("ARCH" === courseCode) || ("ART" === courseCode) || ("ARTD" === courseCode) || ("ARTE" === courseCode) || ("ARTF" === courseCode) || ("ARTH" === courseCode)  || ("ARTS " === courseCode)  || ("DANC" === courseCode) || ("FAA" === courseCode) || ("LA" === courseCode) || ("MUS" === courseCode) || ("THEA" === courseCode) || ("UP" === courseCode) )
+	{
+		return "rgba(255,191,191,0.8)"; //"#FFBFBF";
+	}
+	// Graduate College
+	else if( ("CIC" === courseCode) || ("GC" === courseCode) || ("PSM" === courseCode) )
+	{
+		return "rgba(184,17,184,0.8)"; //"#B811B8";
+	}
+	// Division of General Studies
+	else if( ("GS" === courseCode) )
+	{
+		return "rgba(100,17,184,0.8)"; //"#6411B8";
+	}
+	// Liberal Arts and Sciences
+	else if( ("AAS" === courseCode) || ("AFRO" === courseCode) || ("AFST" === courseCode) || ("AIS" === courseCode) || ("ANTH" === courseCode) || ("ARAB" === courseCode)  || ("ASST" === courseCode)  || ("ASTR" === courseCode) || ("ATMS" === courseCode) || ("BASQ" === courseCode) || ("BCS" === courseCode) || ("BIOC" === courseCode) || ("BIOL" === courseCode) || ("BIOP" === courseCode) || ("BTW" === courseCode) || ("CATL" === courseCode) || ("CDB" === courseCode) || ("CHBE" === courseCode) || ("CHEM" === courseCode) || ("CHIN" === courseCode) || ("CHP" === courseCode) || ("CLCV" === courseCode) || ("CMN" === courseCode) || ("CW" === courseCode) || ("CZCH" === courseCode) || ("EALC" === courseCode) || ("ECON" === courseCode) || ("EIL" === courseCode) || ("ENGL" === courseCode) || ("ENSU" === courseCode) || ("ENT" === courseCode) || ("ESE" === courseCode) || ("ESL" === courseCode) || ("EURO" === courseCode) || ("FR" === courseCode) || ("GEOG" === courseCode) || ("GEOL" === courseCode) || ("GER" === courseCode) || ("GLBL" === courseCode) || ("GMC" === courseCode) || ("GRK" === courseCode) || ("GRKM" === courseCode) || ("GWS" === courseCode) || ("HCD" === courseCode) || ("HEBR" === courseCode) || ("HIST" === courseCode) || ("HNDI" === courseCode) || ("HUM" === courseCode) || ("IB" === courseCode) || ("ITAL" === courseCode) || ("JAPN" === courseCode) || ("JS" === courseCode) || ("KOR" === courseCode) || ("LAS" === courseCode) || ("LAST" === courseCode) || ("LAT" === courseCode) || ("LING" === courseCode) )
+	{
+		return "rgba(17,184,172,0.8)"; //"#11B8AC";
+	}
+	// Law
+	else if( ("LAW" === courseCode) )
+	{
+		return "rgba(20,252,160,0.8)"; //"#14FCA0";
+	}
+	// Library and Information Sciences
+	else if( ("LIS" === courseCode) )
+	{
+		return "rgba(163,252,20,0.8)"; //"#A3FC14";
+	}
+	// College of Media
+	else if( ("ADV" === courseCode) || ("AGCM" === courseCode) || ("JOUR" === courseCode) || ("MACS" === courseCode) || ("MDIA" === courseCode) )
+	{
+		return "rgba(232,255,117,0.8)"; //"#E8FF75";
+	}
+	// Provost Academic Programs
+	else if( ("INFO" === courseCode) )
+	{
+		return "rgba(227,227,227,0.8)"; //"#E3E3E3";
+	}
+	// School of Labor and Employee Relations
+	else if( ("LER" === courseCode) )
+	{
+		return "rgba(56,224,157,0.8)"; //"#38E09D";
+	}
+	// School of Social Work
+	else if( ("SOCW" === courseCode) )
+	{
+		return "rgba(0,133,102,0.8)"; //"#008566";
+	}
+	// Veterinary Medicine
+	else if( ("CB" === courseCode) || ("PATH" === courseCode) || ("VCM" === courseCode) || ("VM" === courseCode) )
+	{
+		return "rgba(0,252,236,0.8)"; //"#00FCEC";
+	}
+	// else
+	else 
+	{
+		return "rgba(255,36,36,0.8)"; //"#FF2424";
+	}
 }
