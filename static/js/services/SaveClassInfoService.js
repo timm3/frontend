e8c271bd70533bf8699303e124f0a268d6e832bf
Rishi Girish <rishigirish@aol.com>
2014-05-04 16:36:44 -0500

    conflict resolutions.

--- static/js/services/SaveClassInfoService.js
+++ static/js/services/SaveClassInfoService.js
@@ -23,6 +23,57 @@ appServices.service("SaveClassInfo", function(){
 		return classesAdded;
 	};
 	this.pushClass = function(course){
+		for(var i in classesAdded){
+			if(hasOverlappingDays(course.days.trim(), classesAdded[i].days.trim())){
+				console.log(course);
+				console.log(classesAdded[i]);
+				var newStart = stringToTime(course.startTime)
+				var newEnd = stringToTime(course.endTime)
+				var addedStart = stringToTime(classesAdded[i].startTime)
+				var addedEnd = stringToTime(classesAdded[i].endTime)
+				if( (newStart >= addedStart && newEnd <= addedEnd) || 
+					(newStart <= addedStart && newEnd >= addedEnd ) ||
+					(newStart <= addedStart && newEnd <= addedStart) ||
+					(newStart >= addedStart && newStart <= addedEnd) ){
+					alert("This class conflicts with " + classesAdded[i].code + " " + + classesAdded[i].id);
+					return false;
+				}
+			}
+		}
 		classesAdded.push(course);
+		return true;
 	};
+	/*check to see if classes overlap on some days*/
+	function hasOverlappingDays(days1, days2){
+		if(!days1 || !days2){
+			return false;
+		}
+		for(var i = 0; i<days1.length; i++){
+			if( days2.indexOf(days1.charAt(i)) > -1 ){
+				console.log('has similar days');
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/* check to see if the times overlap. */
+	function stringToTime(str){
+		if(!str){
+			return null;
+		}
+		var timeSplit = str.split(":");
+		if(timeSplit.length<2){
+			console.log("stringToTime :");
+			return null;
+		}
+		var hour = parseInt(timeSplit[0])*100;
+		var minutes = parseInt(timeSplit[1]);
+		if(str.indexOf("PM") > 0){
+			if(hour !== 1200){
+				hour+=1200;
+			}
+		}
+		return hour+minutes;
+	}
 });
